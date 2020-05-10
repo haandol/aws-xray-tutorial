24f205a8dba405249a8c5ea6e9f5d4f1d2587909
Dongkyun Lee <dongkyl@amazon.com>
2020-05-11 00:43:10 +0900

    add vincent

--- scripts/posts.sh
+++ scripts/posts.sh
@@ -3,6 +3,7 @@
 SET=$(seq 1 15)
 
 for i in $SET; do
-    http post http://localhost:8080/posts username="haandol" title="title$i" content="content$1"
+    http post http://localhost:8080/posts username="haandol" title="haandol$i" content="haandol$1"
+    http post http://localhost:8080/posts username="vincent" title="vincent$i" content="vincent$1"
     sleep 0.5
 done
